20eb2e706e6a4dbc4952089b032864bf8f0027f7
Швецов Юрий Валентинович <YValShvetsov@sberbank.ru>
2023-12-22 11:14:05 +0300

    basic implement server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,46 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strings"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func contains(s []string, val string) bool {
+	for _, v := range s {
+		if val == v {
+			return true
+		}
+	}
+	return false
+}
+
+func run() error {
+	return http.ListenAndServe(`:8080`, http.HandlerFunc(metricHandler))
+}
+
+func metricHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	pathParts := strings.Split(r.URL.Path, "/")
+	parts := pathParts[2:]
+	if len(parts) < 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	mType := parts[0]
+	allowMetircTypes := []string{`gauge`, `counter`}
+	if !contains(allowMetircTypes, mType) {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ShvetsovYura/metrics-collector
+
+go 1.21.4
