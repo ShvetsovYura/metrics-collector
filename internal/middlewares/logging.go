52d6f32470658144ccd033dd0e5380735e9c2988
ShvetsovYura <shvetsov.yura@hotmail.com>
2024-01-17 22:03:16 +0300

    Дополнено логирование в middleware x

--- internal/middlewares/logging.go
+++ internal/middlewares/logging.go
@@ -4,16 +4,29 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/ShvetsovYura/metrics-collector/internal/custom"
 	"github.com/ShvetsovYura/metrics-collector/internal/logger"
 )
 
 func WithLog(h http.Handler) http.HandlerFunc {
 	logFunc := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		h.ServeHTTP(w, r)
+		data := &custom.RespData{
+			StatusCode: 0,
+			Size:       0,
+		}
+
+		lw := custom.LogResponseWriter{
+			ResponseWriter: w,
+			Data:           data,
+		}
+
+		h.ServeHTTP(&lw, r)
 		duration := time.Since(start)
 
 		logger.Log.Infoln(
+			"status", data.StatusCode,
+			"size", data.Size,
 			"method", r.Method,
 			"url", r.RequestURI,
 			"duration:", duration,
