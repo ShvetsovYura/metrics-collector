554fdf47b1ed21f0c37e105824c071409d08af6b
ShvetsovYura <shvetsov.yura@hotmail.com>
2024-01-29 16:28:33 +0300

    Использование мидлвари из chi для сжатия вместо кастомной реализации

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -5,14 +5,17 @@ import (
 
 	"github.com/ShvetsovYura/metrics-collector/internal/middlewares"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func ServerRouter(s Storage) chi.Router {
 	r := chi.NewRouter()
-	r.Get("/", middlewares.WithLog(middlewares.WithGzip(MetricGetCurrentValuesHandler(s))))
+	r.Use(middleware.Compress(5, "application/json", "text/html"))
+
+	r.Get("/", middlewares.WithLog(MetricGetCurrentValuesHandler(s)))
 	r.Post(fmt.Sprintf("/update/{%s}/{%s}/{%s}", metricType, metricName, metricValue), middlewares.WithLog(MetricUpdateHandler(s)))
 	r.Get(fmt.Sprintf("/value/{%s}/{%s}", metricType, metricName), middlewares.WithLog(MetricGetValueHandler(s)))
-	r.Post("/update/", middlewares.WithLog(middlewares.WithGzip(MetricUpdateHandlerWithBody(s))))
-	r.Post("/value/", middlewares.WithLog(middlewares.WithGzip(MetricGetValueHandlerWithBody(s))))
+	r.Post("/update/", middlewares.WithLog(MetricUpdateHandlerWithBody(s)))
+	r.Post("/value/", middlewares.WithLog(MetricGetValueHandlerWithBody(s)))
 	return r
 }
