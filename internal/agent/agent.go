1e6300cf8d0eb34ac9aba0dded3d5f68183ed33d
ShvetsovYura <shvetsov.yura@hotmail.com>
2024-02-08 21:39:16 +0300

    Отправка метрик батчами из агента

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -35,7 +35,6 @@ func (a *Agent) Run() {
 			a.sendMetrics()
 		}
 	}
-
 }
 
 func (a *Agent) sendMetrics() {
--- internal/agent/metric.go
+++ internal/agent/metric.go
@@ -10,6 +10,8 @@ import (
 
 type Sender interface {
 	Send(string, string)
+	MarshalToJson(string) []byte
+	GetObj(string) models.Metrics
 }
 type metrics map[string]Sender
 type gauge float64
@@ -21,27 +23,59 @@ func NewMetrics(initSize int) metrics {
 	return m
 }
 
+func (m metrics) SendBatch(baseURL string) error {
+	metricsBatch := make([]models.Metrics, 10)
+	for k, v := range m {
+		metricsBatch = append(metricsBatch, v.GetObj(k))
+	}
+	link := fmt.Sprintf("http://%s/updates/", baseURL)
+	data, err := json.Marshal(metricsBatch)
+	if err != nil {
+		return err
+	}
+	sendMetric(data, link, "application/json")
+
+}
+
 func (g gauge) Send(mName string, baseURL string) {
 	link := fmt.Sprintf("http://%s/update/", baseURL)
-	val := float64(g)
-	obj := models.Metrics{
-		ID:    mName,
-		MType: "gauge",
-		Value: &val,
-	}
-	data, _ := json.Marshal(obj)
 
+	data := g.MarshalToJson(mName)
 	sendMetric(data, link, "application/json")
+}
+
+func (g gauge) MarshalToJson(mName string) []byte {
+	data, _ := json.Marshal(g.GetObj())
+	return data
 }
 
 func (c counter) Send(mName string, baseURL string) {
 	link := fmt.Sprintf("http://%s/update/", baseURL)
+	data := c.MarshalToJson(mName)
+	sendMetric(data, link, "application/json")
+}
+
+func (c counter) MarshalToJson(mName string) []byte {
+	data, _ := json.Marshal(c.GetObj())
+	return data
+}
+
+func (c counter) GetObj(mName string) models.Metrics {
 	val := int64(c)
-	data, _ := json.Marshal(models.Metrics{
-		ID:    counterMetricFieldName,
+
+	return models.Metrics{
+		ID:    mName,
 		MType: internal.InCounterName,
 		Delta: &val,
-	})
-	sendMetric(data, link, "application/json")
+	}
+}
 
+func (g gauge) GetObj(mName string) models.Metrics {
+	val := float64(g)
+
+	return models.Metrics{
+		ID:    mName,
+		MType: internal.InGaugeName,
+		Value: &val,
+	}
 }
